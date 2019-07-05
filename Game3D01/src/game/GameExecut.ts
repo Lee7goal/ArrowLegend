7bea62c87a348a7273c02843c8ab9f44bd20463f
hk123123 <hk123123@kuwan511.com>
2019-07-05 17:05:22 +0800

    增加总体游戏时间轴

---
+++ Game3D01/src/game/GameExecut.ts
@@ -0,0 +1,52 @@
+import Game from "./Game";
+
+export default class GameExecut extends Laya.EventDispatcher {
+
+    now:number = 0;
+    st:number  = 0;
+    stopt:number = 0;
+    startt:number = 0;
+    dt:number = 0;//时间轴静止时间
+
+    constructor(){
+        super();
+        this.now = Laya.Browser.now();
+        this.st  = this.now;
+        this.stop_();
+    }
+
+    public start():void{
+        this.now = Laya.Browser.now();
+        this.dt += ( this.now - this.stopt) ;
+        var arr = Game.AiArr;
+        for (let i = 0; i < arr.length; i++) {
+            if( arr[i].animator ) arr[i].animator.speed = 1;
+        }
+        Laya.stage.frameLoop(1,this,this.ai);
+    }
+
+    public stop_():void{
+        this.now = Laya.Browser.now();
+        this.stopt = this.now;
+        Laya.timer.clear(this,this.ai);
+        //animator
+        var arr = Game.AiArr;
+        for (let i = 0; i < arr.length; i++) {
+            if( arr[i].animator ) arr[i].animator.speed = 0;
+        }
+    }
+
+    /**得到游戏世界的时间轴 */
+    public getWorldNow():number{
+        this.now = Laya.Browser.now();
+        return (this.now - this.dt )//; - this.dt;
+    }
+
+    ai():void{
+        var arr = Game.AiArr;
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].ai();
+        }
+    }
+
+}
