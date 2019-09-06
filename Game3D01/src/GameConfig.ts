5abb3017dc0a7ccfe2d97a4d2818991d9904c89e
ntx745120 <ntx745120@163.com>
2019-09-06 21:10:35 +0800

    no message

--- Game3D01/src/GameConfig.ts
+++ Game3D01/src/GameConfig.ts
@@ -10,7 +10,7 @@ export default class GameConfig{
     static screenMode:string="none";
     static alignV:string="top";
     static alignH:string="left";
-    static startScene:any="test/Reborth.scene";
+    static startScene:any="game/battleIndexBox.scene";
     static sceneRoot:string="";
     static debug:boolean=false;
     static stat:boolean=true;
