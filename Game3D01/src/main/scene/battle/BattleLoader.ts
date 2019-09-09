df9eeb8456db9ca7ace673b0affde36b980e5c11
ntx745120 <ntx745120@163.com>
2019-09-09 12:25:32 +0800

    改了三个地图

--- Game3D01/src/main/scene/battle/BattleLoader.ts
+++ Game3D01/src/main/scene/battle/BattleLoader.ts
@@ -130,7 +130,8 @@ export default class BattleLoader {
         }
         else
         {
-            if (this._index > 10) {
+            let maxCeng:number =  SysMap.getTotal(Session.gameData.chapterId);
+            if (this._index > maxCeng) {
                 this._index = 1;
             }
             this._mapId = Session.homeData.chapterId * 1000 + this._index;
@@ -142,7 +143,7 @@ export default class BattleLoader {
             let configId: number = Number(configArr[Math.floor(configArr.length * Math.random())]);
             this._configId = configId;
         }
-        // this._configId = 101401;
+        // this._configId = 102001;
         console.log("当前地图", this._mapId, this._configId);
         Laya.loader.load("h5/mapConfig/" + this._configId + ".json", new Laya.Handler(this, this.loadBg));
     }
